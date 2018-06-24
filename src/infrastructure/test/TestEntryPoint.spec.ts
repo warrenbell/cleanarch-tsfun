e2bcd16ded6a54dac70c02810dcac2c643b0369f
warrenbell <warrenbell2@gmail.com>
2018-06-23 21:32:11 -0700

    Commit 1

--- src/infrastructure/test/TestEntryPoint.spec.ts
+++ src/infrastructure/test/TestEntryPoint.spec.ts
@@ -11,13 +11,16 @@ describe('Testing entry point into delivery layer', () => {
     describe('createWidgetRequestImp(widget)', () => {
         it('should return an object via createWidgetViewModelImp with certain properties', () => {
 
-            // This code would be somewhere in level 4 in a mobile app, web app, cli, api, etc.
+            // This code would be somewhere in level 4 infrastructure in a mobile app, web app, cli, api, etc.
+            // Implementation of CreateWidgetViewModelInt (ViewModel) interface
             const createWidgetViewModelImp: CreateWidgetViewModelInt = (widget) => {
                 // Update UI here. Main output point
                 chai.expect(widget).deep.equal({id: "6eu47839ifg", rev: "jd62jcg649fgdy", name: "Widget Name"});
             }
+            // Implementation of CreateWidgetRequestInt (Request) interface
             const createWidgetRequestImp: CreateWidgetRequestInt = createWidgetControllerConstructor(saveWidgetEntityGatewayImp, createWidgetViewModelImp);
             // Called by UI. Main entry point
+            // CreateWidgetControllerImp main createWidget controller function (Controller) implementation
             createWidgetRequestImp({name: "Widget Name"});
 
         });
